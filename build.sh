14858a53fe53565b395b3dfdba854f7794dbc8d3
poleonek <szymoniewazne@gmail.com>
2024-10-16 22:00:42 +0200

    Add -b|--build-type arg to build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,31 @@ set -e
 
 shopt -s expand_aliases
 
-alias clang="clang -g"
+case $1 in
+    -b|--build-type)
+        BUILD_TYPE=$2
+        ;;
+esac
+
+if [[ -z $BUILD_TYPE ]]; then
+    BUILD_TYPE="RELEASE"
+fi
+
+case $BUILD_TYPE in
+    DEBUG)
+        alias clang="clang -O0 -g"
+        ;;
+    RELEASE)
+        alias clang="clang"
+        ;;
+    *)
+        echo "Accepted args for -b|--build-type are only RELEASE or DEBUG."
+        echo "$BUILD_TYPE was provided."
+        exit -1
+        ;;
+esac
+
 
 cd build
+
 clang ../main.c -o main -lSDL3
