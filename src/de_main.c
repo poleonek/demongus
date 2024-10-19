51368d2a50a600f77ac5711bcf5e54d0aa904a00
Mateusz Garbowski <fda0@matg.pl>
2024-10-19 18:33:42 +0200

    WIP collision, handle both axis, ignore missed collisions

--- src/de_main.c
+++ src/de_main.c
@@ -13,7 +13,7 @@ static void Game_Iterate(AppState *app)
         app->dt = delta_time * (0.001f);
         app->dt = Min(app->dt, 1.f); // clamp dt to 1s
     }
-
+    
     // player input
     {
         V2 dir = {0};
@@ -22,18 +22,18 @@ static void Game_Iterate(AppState *app)
         if (app->keyboard[SDL_SCANCODE_A] || app->keyboard[SDL_SCANCODE_LEFT])  dir.x -= 1;
         if (app->keyboard[SDL_SCANCODE_D] || app->keyboard[SDL_SCANCODE_RIGHT]) dir.x += 1;
         dir = V2_Normalize(dir);
-
+        
         float player_speed = 5.f * app->dt;
-
+        
         Assert(app->player_id < ArrayCount(app->object_pool));
         Object *player = app->object_pool + app->player_id;
         player->dp = V2_Scale(dir, player_speed);
         //player->p = V2_Add(player->p, player->dp);
-
+        
         // camera follows the player
         app->camera_p = player->p;
     }
-
+    
     // movement
     {
         float wall_margin = 0.001f;
@@ -46,9 +46,9 @@ static void Game_Iterate(AppState *app)
             Object *obj = app->object_pool + obj_id;
             if (!(obj->flags & ObjectFlag_Move)) continue;
             if (!obj->dp.x && !obj->dp.y) continue;
-
+            
+            // @todo(mg) iterate through multiple collisions - otherwise the player will get into the walls on complex geometry
             bool moved = false;
-
             ForU32(obstacle_id, app->object_count)
             {
                 Object *obstacle = app->object_pool + obstacle_id;
@@ -57,38 +57,46 @@ static void Game_Iterate(AppState *app)
                 // @speed(mg) early exist if obstacle is not in obj movement's bounding box
                 
                 V2 minkowski_half_dim = V2_Scale(V2_Add(obj->dim, obstacle->dim), 0.5f);
-                //V2 obs_min = V2_Sub(obstacle->p, obs_half_dim);
-                //V2 obs_max = V2_Add(obstacle->p, obs_half_dim);
                 
-                if (obj->dp.x)
+                ForU32(main_axis, Axis2_COUNT)
                 {
-                    float near_wall_x = obstacle->p.x + (obj->dp.x < 0.f ? minkowski_half_dim.x : -minkowski_half_dim.x);
-                    float near_wall_y0 = obstacle->p.y - minkowski_half_dim.y;
-                    float near_wall_y1 = obstacle->p.y + minkowski_half_dim.y;
+                    Axis2 other_axis = (main_axis == Axis2_X ? Axis2_Y : Axis2_X);
                     
-                    float obj_wall_distance = near_wall_x - obj->p.x;
-                    float collision_t = obj_wall_distance / obj->dp.x;
-                    
-                    // @todo(mg) special handle case where: collision_t < (1.f - some_epsilon)
-                    // @todo(mg) make sure we handle the case where player is in the wall
-                    if (collision_t > 0.f && collision_t < 1.f)
+                    if (obj->dp.E[main_axis])
                     {
-                        V2 new_p = V2_Add(obj->p, obj->dp);
-                        new_p.x = near_wall_x + (obj->dp.x < 0.f ? wall_margin : -wall_margin);
-                        obj->p = new_p;
-                        moved = true;
-                        break; // @todo find the closest collision instead of find the first
+                        float near_wall_main = obstacle->p.E[main_axis] -
+                            minkowski_half_dim.E[main_axis]*SignF(obj->dp.E[main_axis]);
+                        float near_wall_other0 = obstacle->p.E[other_axis] - minkowski_half_dim.E[other_axis];
+                        float near_wall_other1 = obstacle->p.E[other_axis] + minkowski_half_dim.E[other_axis];
+                        
+                        float obj_wall_distance = near_wall_main - obj->p.E[main_axis];
+                        float collision_t = obj_wall_distance / obj->dp.E[main_axis];
+                        
+                        // @todo(mg) special handle case where: collision_t < (1.f - some_epsilon)
+                        // @todo(mg) make sure we handle the case where player is in the wall
+                        
+                        if (collision_t > 0.f && collision_t < 1.f)
+                        {
+                            V2 new_p = V2_Add(obj->p, obj->dp);
+                            if (new_p.E[other_axis] >= near_wall_other0 &&
+                                new_p.E[other_axis] <= near_wall_other1)
+                            {
+                                new_p.E[main_axis] = near_wall_main - wall_margin*SignF(obj->dp.E[main_axis]);
+                                
+                                obj->p.E[main_axis] = new_p.E[main_axis];
+                                obj->dp.E[main_axis] = 0.f; // @todo(mg) this is not ideal, angled walls will be "sticky", we should calculate new dp vector here
+                                moved = true;
+                                break; // @todo find the closest collision instead of finding the first
+                            }
+                        }
                     }
                 }
             }
-
-            if (!moved)
-            {
-                obj->p = V2_Add(obj->p, obj->dp);
-            }
+            
+            obj->p = V2_Add(obj->p, obj->dp);
         }
     }
-
+    
     // display objects
     {
         float camera_scale = 1.f;
@@ -97,11 +105,11 @@ static void Game_Iterate(AppState *app)
             camera_scale = wh / app->camera_range;
         }
         V2 window_transform = (V2){app->width*0.5f, app->height*0.5f};
-
+        
         ForU32(i, app->object_count)
         {
             Object *obj = app->object_pool + i;
-
+            
             float dim = 20;
             SDL_FRect rect =
             {
@@ -110,26 +118,26 @@ static void Game_Iterate(AppState *app)
                 obj->dim.x,
                 obj->dim.y
             };
-
+            
             // apply camera
             {
                 rect.x -= app->camera_p.x;
                 rect.y -= app->camera_p.y;
-
+                
                 rect.x *= camera_scale;
                 rect.y *= camera_scale;
                 rect.w *= camera_scale;
                 rect.h *= camera_scale;
-
+                
                 rect.x += window_transform.x;
                 rect.y += window_transform.y;
             }
-
+            
             SDL_SetRenderDrawColorFloat(app->renderer, obj->color.r, obj->color.g, obj->color.b, obj->color.a);
             SDL_RenderFillRect(app->renderer, &rect);
         }
     }
-
+    
     // draw mouse
     {
         static float r = 0;
@@ -137,7 +145,7 @@ static void Game_Iterate(AppState *app)
         if (r > 255) r = 0;
         /* set the color to white */
         SDL_SetRenderDrawColor(app->renderer, (int)r, 255 - (int)r, 255, 255);
-
+        
         float dim = 20;
         SDL_FRect rect = {
             app->mouse.x - 0.5f*dim, app->mouse.y - 0.5f*dim,
@@ -161,7 +169,7 @@ static Object *Object_Create(AppState *app, Uint32 flags)
     Assert(app->object_count < ArrayCount(app->object_pool));
     Object *obj = app->object_pool + app->object_count;
     app->object_count += 1;
-
+    
     MemsetZeroStructPtr(obj);
     obj->flags = flags;
     obj->color = ColorF_RGB(1,1,1);
@@ -173,11 +181,11 @@ static Object *Object_Wall(AppState *app, V2 p, V2 dim)
     Object *obj = Object_Create(app, ObjectFlag_Draw|ObjectFlag_Collide);
     obj->p = p;
     obj->dim = dim;
-
+    
     static float r = 0.f;
     r += 0.421f;
     while (r > 1.f) r -= 1;
-
+    
     obj->color = ColorF_RGB(r, .5f, .9f);
     return obj;
 }
@@ -187,7 +195,7 @@ static void Game_Init(AppState *app)
     app->frame_time = SDL_GetTicks();
     app->object_count += 1; // reserve object under index 0 as special 'nil' value
     app->camera_range = 20;
-
+    
     // add player
     {
         Object *player = Object_Create(app, ObjectFlag_Draw|ObjectFlag_Move);
@@ -196,15 +204,15 @@ static void Game_Init(AppState *app)
         player->color = ColorF_RGB(.97f, .09f ,0);
         app->player_id = Object_IdFromPointer(app, player);
     }
-
+    
     // add walls
     {
         float thickness = 0.5f;
         float length = 7.5f;
         float off = length*0.5f - thickness*0.5f;
-        Object_Wall(app, (V2){ off, 0}, (V2){thickness, length});
+        Object_Wall(app, (V2){off, 0}, (V2){thickness, length});
         Object_Wall(app, (V2){-off, 0}, (V2){thickness, length});
-        //Object_Wall(app, (V2){0,  off}, (V2){length, thickness});
-        //Object_Wall(app, (V2){0, -off}, (V2){length, thickness});
+        Object_Wall(app, (V2){0, off}, (V2){length, thickness});
+        Object_Wall(app, (V2){0,-off}, (V2){length*0.5f, thickness});
     }
 }
