22ac05025318304e839688666b86ffec39483994
Mateusz Garbowski <fda0@matg.pl>
2024-10-19 07:40:56 +0200

    animate window background color

--- src/main.c
+++ src/main.c
@@ -6,20 +6,50 @@
 #include <SDL3/SDL.h>
 #include <SDL3/SDL_main.h>
 
-
 #define WINDOW_HEIGHT 480
 #define WINDOW_WIDTH 640
 
 typedef struct
 {
+    // SDL, window stuff
     SDL_Window* window;
     SDL_Renderer* renderer;
     int width, height;
+
+    // time
+    Uint64 frame_time;
+    float dt;
 } AppState;
 
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
-    AppState* state = (AppState*)appstate;
+    AppState* app = (AppState*)appstate;
+
+    {
+        Uint64 new_frame_time = SDL_GetTicks();
+        Uint64 delta_time = new_frame_time - app->frame_time;
+        app->frame_time = new_frame_time;
+        app->dt = delta_time * (0.001f);
+    }
+
+    {
+        static float r = 0;
+        r += app->dt * 30.f;
+        if (r > 255) r = 0;
+        SDL_SetRenderDrawColor(app->renderer, (int)r, 0, 0, 255);
+
+        /* you have to draw the whole window every frame. Clearing it makes sure the whole thing is sane. */
+        SDL_RenderClear(app->renderer);  /* clear whole window to that fade color. */
+
+        /* set the color to white */
+        SDL_SetRenderDrawColor(app->renderer, 255, 255, 255, 255);
+
+        /* draw a square where the mouse cursor currently is. */
+        //SDL_RenderFillRect(app->renderer, &mouseposrect);
+
+        /* put everything we drew to the screen. */
+        SDL_RenderPresent(app->renderer);
+    }
 
     return SDL_APP_CONTINUE;
 }
@@ -34,6 +64,15 @@ SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
         {
             return SDL_APP_SUCCESS;
         } break;
+
+        case SDL_EVENT_KEY_UP:
+        case SDL_EVENT_KEY_DOWN:
+        {
+            if (event->key.key == SDLK_ESCAPE) {
+                return SDL_APP_SUCCESS; // useful in development
+            }
+        } break;
+
         default:
         {
         } break;
@@ -59,6 +98,7 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char** argv)
     }
     state->width = WINDOW_WIDTH;
     state->height = WINDOW_HEIGHT;
+    state->frame_time = SDL_GetTicks();
 
     if (!SDL_CreateWindowAndRenderer("demongus", state->width, state->height, SDL_WINDOW_RESIZABLE, &state->window, &state->renderer))
     {
